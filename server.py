65dc2578918e2d55ac7458466e94a91434498027
mertdemirbas <mertdemirbas@users.noreply.github.com>
2025-04-17 16:17:28 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,69 +1,20 @@
-from flask import Flask, jsonify
-from pyhoroscope import Horoscope
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+from pyhoroscope import get_today_horoscope
 
-app = Flask (__name__)
+app = Flask(__name__)
 CORS(app)
 
-############################################
-# Index 
-############################################
+@app.route("/")
+def index():
+    return "Horoscope API çalışıyor."
 
-@app.route ('/', methods=['GET'])
-def index_route () :
-    return jsonify({
-		'author' : 'Tapasweni Pathak',
-		'author_url' : 'http://tapasweni-pathak.github.io/',
-		'base_url' : 'horoscope-api.herokuapp.com',
-	    	'project_name' : 'Horoscope API',
-		'project_url' : 'http://tapasweni-pathak.github.io/Horoscope-API'
-	})
+@app.route("/horoscope/today/<sign>", methods=["GET"])
+def today_horoscope(sign):
+    result = get_today_horoscope(sign.lower())
+    return jsonify(result)
 
-
-############################################
-# Horoscopes
-###########################################
-
-#Todays' Horoscope
-@app.route ('/horoscope/today/<sunsign>', methods=['GET'])
-def today_horoscope_route (sunsign) :
-	result = dict (Horoscope.get_todays_horoscope (sunsign))
-	return jsonify (date=result['date'],
-			sunsign=result['sunsign'],
-			horoscope=result['horoscope'])
-					
-
-#Current Week Horoscope
-@app.route ('/horoscope/week/<sunsign>', methods=['GET'])
-def weekly_horoscope_route (sunsign) :
-	result = dict (Horoscope.get_weekly_horoscope (sunsign))
-	return jsonify (week=result['week'],
-			sunsign=result['sunsign'],
-			horoscope=result['horoscope'])
-
-#Current Month Horoscope
-@app.route ('/horoscope/month/<sunsign>', methods=['GET'])
-def monthly_horoscope_route (sunsign) :
-	result = dict (Horoscope.get_monthly_horoscope (sunsign))
-	return jsonify (month=result['month'],
-			sunsign=result['sunsign'],
-			horoscope=result['horoscope'])
-
-#Current Year Horoscope
-@app.route ('/horoscope/year/<sunsign>', methods=['GET'])
-def yearly_horoscope_route (sunsign) :
-	result = dict (Horoscope.get_yearly_horoscope (sunsign))
-	return jsonify (year=result['year'],
-			sunsign=result['sunsign'],
-			horoscope=result['horoscope'])
-
-
-###########################################
-#Start Flask
-###########################################
-
+# Sunucuyu dış dünyaya aç
 import os
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-
